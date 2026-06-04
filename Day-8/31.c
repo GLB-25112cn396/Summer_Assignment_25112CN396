92bc0c43b6a66ad4dbb6b63e22667f6c30f1e498
GLB-25112cn396 <ganeshpachouri321@gmail.com>
2026-06-04 19:21:55 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/31.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;   //pyramid of letters
+    for(i=65;i<=69;i++)
+    {
+        for(j=65;j<=i;j++)
+        {
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
